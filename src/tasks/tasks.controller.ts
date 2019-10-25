a63bc69f83e7fcb02b0cfe22628d80803a436c79
shady-abdelhamid <shady.abdelhamid@outlook.com>
2019-10-25 16:34:27 +0200

    Add delete task
    1. add delete task to service.
    2. add delete task handler to controller.

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task } from './task.modal';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -20,5 +20,10 @@ export class TasksController {
     @Post()
     createTask(@Body() createTaskDto: CreateTaskDto): Task {
         return this.tasksServise.createTask(createTaskDto);
+    }
+
+    @Delete('/:id')
+    deleteTask(@Param('id') id: string) {
+        this.tasksServise.deleteTask(id);
     }
 }
