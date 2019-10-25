616c32e5e76eb1941b0255d12fc43f14f722144c
shady-abdelhamid <shady.abdelhamid@outlook.com>
2019-10-26 01:05:37 +0200

    Add update task status
    1. add update task status handler.
    2. add update task status method in service.

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,6 +1,6 @@
-import { Controller, Get, Post, Body, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete, Patch } from '@nestjs/common';
 import { TasksService } from './tasks.service';
-import { Task } from './task.modal';
+import { Task, TaskStatus } from './task.modal';
 import { CreateTaskDto } from './dto/create-task.dto';
 
 @Controller('tasks')
@@ -25,5 +25,12 @@ export class TasksController {
     @Delete('/:id')
     deleteTask(@Param('id') id: string) {
         this.tasksServise.deleteTask(id);
+    }
+
+    @Patch('/:id/status')
+    updateTaskStatus(
+        @Param('id') id: string,
+        @Body('status') status: TaskStatus): Task {
+        return this.tasksServise.updateTaskStatus(id, status);
     }
 }
