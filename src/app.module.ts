a884ecbf6a619047ea6a152292ec576d44f247aa
shady-abdelhamid <shady.abdelhamid@outlook.com>
2019-11-03 10:13:06 +0200

    Generate auth module
    1. add auth module.
    2. add auth controller.
    3. add auth service.
    4. add user entity.
    5. add user repository.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     TasksModule,
+    AuthModule,
     TypeOrmModule.forRoot(),
   ],
 })
